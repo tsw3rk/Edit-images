3c73ff6b1127e86517e1aa1bc0409a052ef9b366
ranaltodraganlorbmvmellstroy <artemm2007m@gmail.com>
2026-01-29 10:20:24 +0300

    Update filters.h
    тк мы пропустили 2 фильтра , думаю стоит их убрать из кода (не думаю что добавлять их сейчас будет честно)

--- filters.h
+++ filters.h
@@ -6,10 +6,8 @@
 void filter_grayscale(Image*,void*);
 void filter_negative(Image*,void*);
 void filter_crop(Image*,void*);
-void filter_sharpen(Image*,void*);
 void filter_edge(Image*,void*);
 void filter_median(Image*,void*);
-void filter_blur(Image*,void*);
 void filter_glass(Image*,void*);
 void filter_crystallize(Image*,void*);
 void filter_mosaic(Image*,void*);
